6defc65a937deb14b85273b4f402caf0c7c69b45
Aravind1229 <aviaravind1229@gmail.com>
2022-03-09 10:18:27 +0530

    REST

--- webservices/src/main/java/com/rest/webservices/users/User.java
+++ webservices/src/main/java/com/rest/webservices/users/User.java
@@ -1,14 +1,22 @@
 package com.rest.webservices.users;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 @ApiModel(description = "All details about the user")
+@Entity
 public class User {
+@Id
+@GeneratedValue
 private Integer id;
 @ApiModelProperty(notes="Name should have minimum 2 characters")
 @Size(min = 2, message="Name should have minimum 2 characters")
@@ -16,6 +24,8 @@ private String name;
 @ApiModelProperty(notes="Birth date should be in the past")
 @Past
 private Date birth_date;
+@OneToMany(mappedBy = "user")
+private List<Post> posts;
 
 public User(Integer id, String name, Date birth_date) {
 	super();
@@ -40,6 +50,12 @@ public Date getBirth_date() {
 }
 public void setBirth_date(Date birth_date) {
 	this.birth_date = birth_date;
+}
+public List<Post> getPosts() {
+	return posts;
+}
+public void setPosts(List<Post> posts) {
+	this.posts = posts;
 }
 @Override
 public String toString() {
---
+++ webservices/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+insert into user values(1001,sysdate(),'Aravind')
+insert into user values(1002,sysdate(),'jack')
+insert into user values(1003,sysdate(),'jill')
+insert into post values(2001,'My First Post',1001)
+insert into post values(2002,'My Second Post',1001)
